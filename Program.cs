029dec65671065b203816dee0803656e8b2c5f5d
Beatriz Fernandes <93793289+Beatrizfernan@users.noreply.github.com>
2022-10-31 21:32:54 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace enercicio_raio_circulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o o raio do círculo");
+
+            double R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            double pi = 3.14159;
+
+            double A = pi * Math.Pow(R, 2);
+            Console.WriteLine("Área=" + A.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
